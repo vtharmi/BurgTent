2a0e6388c56276459a5c85da479a762cc63b80cd
Thami Vijayakumaran <thami@Thamis-MacBook-Pro.local>
2020-04-22 23:21:53 +0530

    commit after changes

--- server.js
+++ server.js
@@ -20,18 +20,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     res.setHeader(
-//         "Access-Control-Allow-Methods", 
-//         "PUT,DELETE, POST, GET, OPTIONS"
-//     )
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods", 
+        "PUT,DELETE, POST, GET, OPTIONS"
+    )
+    next();
+  });
 
 
 // app.options('/users/storeRecipes', cors())
